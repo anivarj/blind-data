ec251594e753cc86e9e58b8aea53bdd6cf17d53c
Ani Michaud (Varjabedian) <32859488+anivarj@users.noreply.github.com>
2020-12-15 10:55:48 -0600

    took out jupyter syntax to clean up

--- Blinding.py
+++ Blinding.py
@@ -1,57 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# **Author:** Ani Michaud (amichaud3@wisc.edu)
-# <br><br>
-# **Description:** 
-# <br>
-# This script should take a folder of .tif files and blind the data by creating a unique, 5-letter code for the file name and renaming the files. This script assumes you have python3 installed. The order of the script is as follows:
-# * Ask the user where their data is stored (targetWorkspace). It can be in individual sub-folders or just copied into one parent folder
-# * Create an output directory called 'blinded'. 
-# * Make a list of all the files in the targetWorkspace, and import them into a pandas DataFrame
-# * Create a 5-letter random code-name (ending in .tif) for each file path in the DataFrame
-# * Copies the original file to the output 'blinded' folder and renames it to the blinded code-name
-# * Creates a key .csv file for later unblinding
+'''
+Author:Ani Michaud (amichaud3@wisc.edu)
 
-# # Import modules and shit
+Description:
+This script should take a folder of .tif files and blind the data by creating a unique, 5-letter code for the file name and renaming the files. 
+This script assumes you have python3 installed. The order of the script is as follows:
+* Ask the user where their data is stored (targetWorkspace). It can be in individual sub-folders or just copied into one parent folder
+* Create an output directory called 'blinded'. 
+* Make a list of all the files in the targetWorkspace, and import them into a pandas DataFrame
+* Create a 5-letter random code-name (ending in .tif) for each file path in the DataFrame
+* Copies the original file to the output 'blinded' folder and renames it to the blinded code-name
+* Creates a key .csv file for later unblinding
+'''
 
-# In[1]:
-
-
+# Import modules and shit
 import os
 import pandas as pd
 from tkinter.filedialog import askdirectory
 
 
-# # Choose your raw data location
-
-# In[2]:
-
-
+# Choose your raw data location
 targetWorkspace = askdirectory(initialdir='~/', message='SELECT YOUR DATA LOCATION') 
 
 
-# # Create the output folder
-
-# In[3]:
-
-
+# Create the output folder
 output = os.path.join(targetWorkspace,'blinded') 
 os.mkdir(output)
 
 
-# # Meat and potatoes
-
-# ## Get full paths to original files 
-
-# In[4]:
-
-
-origPaths = [] # future list of paths to all original files
-
-
-# In[5]:
-
+# Get full paths to original files 
+origPaths = [] 
 
 #This section gets a list of files inside targetWorkspace and adds the full paths to origPaths list
 for dirpath, dirnames, files in os.walk(targetWorkspace): #walks through targetWorkspace
@@ -62,25 +42,15 @@ for dirpath, dirnames, files in os.walk(targetWorkspace): #walks through targetW
         origPaths.append(os.path.join(dirpath, file))     #for each file, get the full path to it's location
 
 
-# ## Create dataframe
-
-# In[6]:
-
-
+# Create dataframe
 blindedDf = pd.DataFrame(origPaths, columns=["Original Paths"]) #initiate dataFrame with the origPaths list
 pd.set_option('display.max_colwidth', None)                     #sets to display the full column width
 
 
 # ## Create blinded filenames
 
-# In[7]:
-
-
 from random import choice
 from string import ascii_uppercase
-
-
-# In[8]:
 
 
 randomizedPaths = [] #creates empty list for the randomized paths
@@ -89,29 +59,14 @@ randomizedPaths = [] #creates empty list for the randomized paths
 for r in range(0, len(blindedDf)): 
     randomizedPaths.append(os.path.join(output, ''.join(choice(ascii_uppercase) for i in range(5)) + ".tif")) #makes a full path for the randomized name in the output folder
 
-
-# In[9]:
-
-
 blindedDf['Randomized Paths'] = pd.DataFrame(randomizedPaths) #appends the list of randomized paths to the current DataFrame
 
 
 # ## Blind the data
 
-# In[10]:
-
-
 newList = list(zip(blindedDf['Original Paths'], blindedDf['Randomized Paths'])) #zipping the two columns from the dataFrame into a list of tuples 
 
-
-# In[11]:
-
-
 from shutil import copyfile
-
-
-# In[12]:
-
 
 #for each tuple pair, copy the original file to the randomized path
 for orig, randomized in newList:
@@ -120,14 +75,7 @@ for orig, randomized in newList:
 
 # ## Export the key
 
-# In[13]:
-
-
 csvPath = os.path.join(output, 'blinded-key.csv') #create a path for the exported dataframe
-
-
-# In[14]:
-
 
 blindedDf.to_csv(csvPath, index=False) #export the dataframe as csv
 
